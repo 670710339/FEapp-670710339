8e92cd708bb961c6c65759d4e62ff0f0dad9ce70
670710339 <Meesin_s2@silpakorn.edu>
2026-09-10 05:34:53 +0000

    Movie

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,10 +1,27 @@
+import MovieGallery from "../Component/MovieGallery";
+import MovieCard from "../Component/MovieCard";
+import { useState } from "react";
+
 function Home() {
   return (
     <div className="p-8 text-center">
       <h1 className="text-3xl font-bold text-slate-800">🏠 หน้าแรก</h1>
       <p className="mt-2 text-slate-500">ยินดีต้อนรับสู่ MovieHub</p>
-    </div>
+
+      <div className="mx-auto mt-8 max-w-5xl">
+        <MovieCard title="inline mandatorian and Grogu" year={2026} />
+        <MovieCard title="Your Name." year={2016} />
+        <MovieCard title="Spider-Man: Brand New Day" year={2026} />
+      </div>
+      <h2 className="mt-8 text-2xl font-bold text-slate-800">ดูหนังเพิ่มเติม</h2>
+      <div classname="mx-auto mt-8 max-w-md">
+        <MovieGallery />
+      </div>
+    </div >
   );
 }
 
 export default Home;
+
+
+
--- my-app/src/pages/Movies.jsx
+++ my-app/src/pages/Movies.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { movies } from '../data';
+import { movies } from '../Data/data';
 
 function Movies() {
   return (
